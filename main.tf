6de86c3d2a02855b0c336d0d8611bdd71e4d2504
venkatesh-pogula <venkateshpogula95@gmail.com>
2022-08-11 16:35:53 +0530

    adde the jenkinsfile

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
-
-module "ec2_creation" {
-  source = "https://github.com/terraform-aws-modules/terraform-aws-ec2-instance/tree/v2.13.0/examples/basic"
-  version = "2.13.0"   
+module "ec2-instance_example_basic" {
+  source  = "terraform-aws-modules/ec2-instance/aws//examples/basic"
+  version = "2.13.0"
 }
+
